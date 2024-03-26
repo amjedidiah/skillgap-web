d9aec3274a08d763b0c2cdbdbb5ac779ff9f0d65
Jedidiah Amaraegbu <imunacode@gmail.com>
2024-03-26 19:22:44 +0100

    feat(app): 7 better mobile header design

--- src/app/components/landing/landing-header-nav.tsx
+++ src/app/components/landing/landing-header-nav.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { cn } from "@/lib/utils";
 import { Link } from "react-scroll";
 
 const navLinks = [
@@ -16,9 +17,22 @@ const navLinks = [
   },
 ];
 
-export default function LandingHeaderNav() {
+type Props = {
+  isOpen: boolean;
+};
+
+export default function LandingHeaderNav({ isOpen }: Props) {
   return (
-    <ul className="flex items-center justify-center gap-4 lg:gap-8 row-start-2 col-span-2">
+    <ul
+      className={cn(
+        {
+          "max-sm:hidden": !isOpen,
+          "max-sm:absolute right-0 left-0 top-[104px] max-sm:bg-white max-sm:py-4 max-sm:px-6 max-sm:flex-col max-sm:items-end max-sm:shadow-md":
+            isOpen,
+        },
+        "flex items-center justify-center gap-4 lg:gap-8 row-start-2 col-span-2"
+      )}
+    >
       {navLinks.map(({ title, href }) => (
         <li key={title}>
           <Link
--- src/app/components/landing/landing-header.tsx
+++ src/app/components/landing/landing-header.tsx
@@ -1,15 +1,28 @@
+"use client";
 import { SGLogo } from "@/lib/icons";
 import LandingHeaderNav from "@/components/landing/landing-header-nav";
+import { RiMenu3Line } from "react-icons/ri";
+import { useCallback, useState } from "react";
 
 export default function LandingHeader() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = useCallback(() => setIsMenuOpen((prev) => !prev), []);
+
   return (
     <header className="fixed left-0 top-0 w-full z-10 bg-white border-b border-grey-100">
       <div className="container">
-        <div className="grid grid-rows-2 grid-cols-2 sm:flex justify-between items-center pb-0 pt-4 sm:py-6 sm:gap-8">
+        <div className="flex justify-between items-center py-6 gap-6 sm:gap-8">
           <SGLogo />
-          <LandingHeaderNav />
-          <button className="outline-none max-sm:p-2 py-4 px-6 rounded-[2rem] border border-twitter-blue bg-twitter-blue text-white">
+          <LandingHeaderNav isOpen={isMenuOpen} />
+          <button className="outline-none px-6 py-4 rounded-[2rem] border border-twitter-blue bg-twitter-blue text-white">
             Download App
+          </button>
+          <button
+            className="sm:hidden text-2xl border border-grey py-3 px-4 rounded-md text-grey"
+            aria-label="menu"
+            onClick={toggleMenu}
+          >
+            <RiMenu3Line />
           </button>
         </div>
       </div>
